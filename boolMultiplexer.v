4019a46c108e51e652c8a7c868cf50988e024ad0
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-10 08:26:40 -0400

    fixed logic

--- boolMultiplexer.v
+++ boolMultiplexer.v
@@ -13,8 +13,8 @@ module boolMultiplexer(
     
     assign I0 = ~C & D;
     assign I1 = ~C | D;
-    assign I2 = ~C;
-    assign I3 = ~C & D;
+    assign I2 = ~C & D;
+    assign I3 = ~C;
     
     fourToOneMultiplexer mux1(S1,S0,I0,I1,I2,I3,1'b1,Y);
     
